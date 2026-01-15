aa2d3b3f0ffda2433633fbf99d5450898da7dda2
HadasHattab <hadas.htech@gmail.com>
2026-01-15 16:56:33 +0200

    add config for css

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import path from 'path';
 
 // https://vite.dev/config/
@@ -10,6 +11,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    cssInjectedByJsPlugin(),
   ],
   resolve: {
     alias: {
@@ -17,5 +19,33 @@ export default defineConfig({
       '@shared': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    modules: {
+      // שמות מחולקים עבור CSS/SCSS Modules
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    },
+    preprocessorOptions: {
+      scss: {
+        // אם יש משתנים גלובליים או mixins, אפשר לייבא אוטומטית כאן
+        // additionalData: `@import "src/styles/variables.scss";`
+      },
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MultiChannelCore',
+      fileName: (format) => `multi-channel-core.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'], // לא לכלול את React bundle
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    }
+  }
 })
 
