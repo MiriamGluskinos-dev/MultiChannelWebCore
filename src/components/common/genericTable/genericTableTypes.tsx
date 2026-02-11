e31e39869f2d77ec8b76248bde5c3aa2710df7cf
Shira_M <99shipol@gmail.com>
2026-02-11 12:03:44 +0200

    add genericTable Component

---
+++ src/components/common/genericTable/genericTableTypes.tsx
@@ -0,0 +1,13 @@
+export interface ColumnDef {
+  id: string;
+  name: string;
+  slotted?: boolean; // האם העמודה מכילה תוכן מיוחד (כמו קישורים או כפתורים) שדורש טיפול שונה בעיצוב
+  sortable?: boolean;
+  type?: "number" | "string" | "date" | "link" | "icon" | "amount" | "print";
+  href?: string; // רק עבור type "link"
+}
+
+export interface TransactionRow {
+  PaymentID: string;
+  [key: string]: any;
+}
